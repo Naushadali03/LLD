6c0bf253c73aaf6c35c6607ff12f0d2267ad016b
Naushad Ali <naushadshaikhjintur@gmail.com>
2025-10-08 00:14:25 +0530

    elevator design

--- ElevatorLLD/ElevatorDoor.java
+++ ElevatorLLD/ElevatorDoor.java
@@ -1,5 +1,15 @@
 package ElevatorLLD;
 
+
 public class ElevatorDoor {
-    
+
+    public void openDoor(){
+        System.out.println("Opening the Elevator door ");
+    }
+
+    public void closeDoor(){
+        System.out.println("Closing the Elevator door");
+    }
+
 }
+
